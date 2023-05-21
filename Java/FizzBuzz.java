d34cd8370dfce8df7995a4b810ea0037edecb42b
Mangoccino <mickey.chen19@gmail.com>
2023-05-21 14:07:18 +0930

    Updated FizzBuzz solution and learning outcomes.

--- Java/FizzBuzz.java
+++ Java/FizzBuzz.java
@@ -41,3 +41,30 @@ public class FizzBuzz
         }
     }
 }
+
+//Solution code with comments using List<String>
+
+/*
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+    public static List<String> fizzBuzz(int n) {
+        List<String> results = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                results.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                results.add("Fizz");
+            } else if (i % 5 == 0) {
+                results.add("Buzz");
+            } else {
+                results.add(String.valueOf(i));
+            }
+        }
+
+        return results;
+    }
+}
+*/
